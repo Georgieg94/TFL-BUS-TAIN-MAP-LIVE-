f73412f0d7a519a1a945903fc69c261e377e1247
Georgieg94 <georgiegriffin94@aol.com>
2026-09-11 13:43:08 +0100

    Add native TfLLive Xcode project

---
+++ TfLLive/TfLLive/TfLLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TfLLiveApp.swift
+//  TfLLive
+//
+//  Created by Georgie Griffin on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TfLLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TfLLive/TfLLiveTests/TfLLiveTests.swift
@@ -0,0 +1,19 @@
+//
+//  TfLLiveTests.swift
+//  TfLLiveTests
+//
+//  Created by Georgie Griffin on 11/09/2026.
+//
+
+import Testing
+@testable import TfLLive
+
+struct TfLLiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
